d2261d2ed29346b9f1deff3684c5d4495b6ad62d
Volodymyr <volodcka96321@gmail.com>
2025-07-10 16:45:41 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const threeDaysDiscount = 20;
+  const sevenDaysDiscount = 50;
+  const dailyRate = 40;
+  let totalCost = 0;
+
+  if (days < 1) {
+    throw new Error('Days must be at least 1');
+  }
+
+  if (days >= 7) {
+    totalCost = days * dailyRate - sevenDaysDiscount;
+  } else if (days >= 3) {
+    totalCost = days * dailyRate - threeDaysDiscount;
+  } else {
+    totalCost = days * dailyRate;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
